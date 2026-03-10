60a764d26309440eb7a7e6272c18eec92238011f
Raf Demets <raf.demets@vdabcampus.be>
2026-03-10 21:56:15 +0100

    cross origin

--- src/main/java/com/springinitrenderdrone2/renderdrone2/ResultaatController.java
+++ src/main/java/com/springinitrenderdrone2/renderdrone2/ResultaatController.java
@@ -4,6 +4,7 @@
  */
 package com.springinitrenderdrone2.renderdrone2;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author rafde
  */
 @RestController
+@CrossOrigin
 public class ResultaatController {
 
     public ResultaatController(ResultaatRequests resultaatRequests) {
